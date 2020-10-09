23007e3af3e52668f939b9b35ddd59cee245ca6c
xushunyu <2631582891@qq.com>
2020-10-09 15:06:52 +0800

    数据适配器已完成

---
+++ app/src/main/java/com/example/recyclerviewdemo/FruitAdapter.kt
@@ -0,0 +1,39 @@
+package com.example.recyclerviewdemo
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import java.text.SimpleDateFormat
+import java.util.*
+
+class FruitAdapter(private val fruit:List<Fruit>): RecyclerView.Adapter<FruitAdapter.ViewHolder>() {
+    inner class ViewHolder(view: View): RecyclerView.ViewHolder(view) {
+        val fruitImage: ImageView = view.findViewById(R.id.fruitImage)
+        val fruitName: TextView = view.findViewById(R.id.fruitName)
+        val fruitTime: TextView = view.findViewById(R.id.fruitTime)
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+       val view = LayoutInflater.from(parent.context).inflate(R.layout.fruit_item, parent, false)
+        return ViewHolder(view)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val fruit = fruit[position]
+        holder.fruitImage.setImageResource(fruit.imageId)
+        holder.fruitName.text=fruit.name
+        holder.fruitTime.text= getTime()
+    }
+
+    private fun getTime(): String? {
+        val date = Date()
+        val time = SimpleDateFormat("HH:mm", Locale.CHINA)
+        return time.format(date)
+    }
+
+    override fun getItemCount()=fruit.count()
+
+}
